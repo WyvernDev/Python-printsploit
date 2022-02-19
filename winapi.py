b39dc1b9f636a1804e8cd9f5b5c06db27e07c959
DeepPain <56605401+DeepPainDev@users.noreply.github.com>
2022-02-19 00:24:51 +0000

    add comments

--- winapi.py
+++ winapi.py
@@ -1,3 +1,7 @@
+'''
+This is a simple file to get the windows native functions so we can write to the memory and create a remote thread when we need to.
+I would've used a memory library in python but i dont know of any python mem libs that allow me to create remote threads so I would've needed to use ctypes eventually
+'''
 import ctypes
 import win32con
 import win32process
